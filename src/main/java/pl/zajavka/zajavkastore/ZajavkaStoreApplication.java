2e81b8be8e80ef7aff3eed75748189a0794440a4
ignacy (air) <ignacyszpilowski@gmai.com>
2023-06-13 10:51:32 +0200

    JPA - QueryByExample

--- src/main/java/pl/zajavka/zajavkastore/ZajavkaStoreApplication.java
+++ src/main/java/pl/zajavka/zajavkastore/ZajavkaStoreApplication.java
@@ -3,6 +3,7 @@ package pl.zajavka.zajavkastore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.zajavka.zajavkastore.business.NamedQueryService;
+import pl.zajavka.zajavkastore.business.QueryByExampleService;
 import pl.zajavka.zajavkastore.business.SomeService;
 import pl.zajavka.zajavkastore.infrastructure.configuration.ApplicationConfiguration;
 
@@ -11,8 +12,9 @@ public class ZajavkaStoreApplication {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
         NamedQueryService namedQueryService = context.getBean(NamedQueryService.class);
+        QueryByExampleService queryByExampleService = context.getBean(QueryByExampleService.class);
         SomeService someService = context.getBean(SomeService.class);
 
-        someService.call();
+        queryByExampleService.queryByExample();
     }
 }
